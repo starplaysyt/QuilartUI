3598a085af48661f99b856ed51d8b17104d873ef
starplaysyt <horaudmitry@gmail.com>
2026-04-10 16:00:20 +0300

    Deleted playground and debug/research methods in IndexGroup and VertexGroup

--- Graphics/IndexGroup.cs
+++ Graphics/IndexGroup.cs
@@ -19,7 +19,7 @@ public class IndexGroup
 
     public static int GetIndexCount(int vertCount) => 3 * (vertCount - 2);
 
-    public static IndexGroup ConnectSelfFan(GeometryShape owner, VertexGroup vertGroup)
+    public static IndexGroup ConnectFan(GeometryShape owner, VertexGroup vertGroup)
     {
         Logger.LogTrace(
             $"Connecting self-fan for vertex group: {vertGroup.StartIndex} - {vertGroup.StartIndex + vertGroup.Length}");
@@ -43,82 +43,7 @@ public class IndexGroup
         return indexGroup;
     }
 
-    public static IndexGroup ConnectSelfStrip(GeometryShape owner, VertexGroup vertGroup)
-    {
-        var vertLength = vertGroup.Length;
-        var vertIndex = vertGroup.StartIndex;
-
-        Logger.LogTrace($"Connecting self-strip for vertex group: {vertLength} - {vertIndex + vertLength}");
-
-        var indexGroup = new IndexGroup(owner);
-        var indexCount = GetIndexCount(vertGroup.Length);
-
-        indexGroup.Length = indexCount;
-
-        Span<int> indices = stackalloc int[indexCount];
-
-        for (var i = 0; i < vertLength - 2; i++)
-        {
-            indices[i * 3 + 2] = vertIndex + i + 2;
-
-            if (i % 2 == 0)
-            {
-                indices[i * 3] = vertIndex + i;
-                indices[i * 3 + 1] = vertIndex + i + 1;
-            }
-            else
-            {
-                indices[i * 3] = vertLength + i + 1;
-                indices[i * 3 + 1] = vertLength + i;
-            }
-        }
-
-        indexGroup.StartIndex = owner.IndexArray.Length;
-        owner.IndexArray.AddSeveral(indices);
-        owner.IndexGroups.Add(indexGroup);
-
-        return indexGroup;
-    }
-
     public static IndexGroup ConnectStrip(GeometryShape owner, VertexGroup group1, VertexGroup group2)
-    {
-        var vert1Length = group1.Length;
-        var vert1Index = group1.StartIndex;
-        var vert2Length = group2.Length;
-        var vert2Index = group2.StartIndex;
-
-        Logger.LogTrace(
-            $"Connecting strip for vertex groups: {vert1Index} - {vert1Index + vert1Length} with {vert2Index} - {vert2Length}");
-
-        var indexGroup = new IndexGroup(owner);
-        var minVertLength = Math.Min(vert1Length, vert2Length);
-        var indexCount = GetIndexCount(minVertLength * 2);
-
-        indexGroup.Length = indexCount;
-
-        Span<int> indices = stackalloc int[indexCount];
-
-        Logger.LogTrace($"INFO: indexCount: {indexCount} vertLength: {minVertLength}");
-        var lastIndex = 0;
-        for (var i = 0; i < minVertLength - 1; i++)
-        {
-            indices[lastIndex++] = vert1Index + i;
-            indices[lastIndex++] = vert1Index + i + 1;
-            indices[lastIndex++] = vert2Index + i + 1;
-
-            indices[lastIndex++] = vert2Index + i;
-            indices[lastIndex++] = vert1Index + i;
-            indices[lastIndex++] = vert2Index + i + 1;
-        }
-
-        indexGroup.StartIndex = owner.IndexArray.Length;
-        owner.IndexArray.AddSeveral(indices);
-        owner.IndexGroups.Add(indexGroup);
-
-        return indexGroup;
-    }
-
-    public static IndexGroup ConnectStripExtra(GeometryShape owner, VertexGroup group1, VertexGroup group2)
     {
         var (vert1Index, vert1Length) = (group1.StartIndex, group1.Length);
         var (vert2Index, vert2Length) = (group2.StartIndex, group2.Length);
@@ -127,9 +52,6 @@ public class IndexGroup
         Span<int> indices = stackalloc int[indexCount];
         var lastIndex = 0;
         var triangleCount = vert1Length + vert2Length - 2;
-        
-        // var step1 = vert1Length > 1 ? 1f / (vert1Length - 1) : 2f;
-        // var step2 = vert2Length > 1 ? 1f / (vert2Length - 1) : 2f;
 
         var step1 = 1f / (vert1Length - 1);
         var step2 = 1f / (vert2Length - 1);
@@ -161,19 +83,13 @@ public class IndexGroup
                 norm2 += step2;
             }
         }
-
-        // for (int i = 0; i < indexCount; i++)
-        // {
-        //     Console.Write(" " + i);
-        //     
-        //     if (i % 3 == 0) Console.Write('|');
-        // }
-
+        
         var indexGroup = new IndexGroup(owner)
         {
             Length = indexCount,
             StartIndex = owner.IndexArray.Length
         };
+        
         owner.IndexArray.AddSeveral(indices);
         owner.IndexGroups.Add(indexGroup);
 
--- Graphics/VertexGroup.cs
+++ Graphics/VertexGroup.cs
@@ -15,80 +15,15 @@ public class VertexGroup
         Owner = owner;
     }
 
-    public static VertexGroup CreateCircle(GeometryShape owner, Point2 location, Color color, float radius)
-    {
-        var group = new VertexGroup(owner);
-        var quality = 32; //(int)(radius * 0.5f);
-        var da = 2.0f * MathF.PI / quality;
-
-        var startIndex = owner.VertexArray.Length;
-        Span<Vertex> pointer = stackalloc Vertex[quality + 1];
-
-        group.StartIndex = startIndex;
-        group.Length = quality + 1;
-
-        for (var i = 0; i <= quality; i++)
-        {
-            var angle = da * i;
-            pointer[i].Position = new Point2(
-                location.X + radius * MathF.Cos(angle),
-                location.Y + radius * MathF.Sin(angle));
-            pointer[i].Color = color;
-        }
-
-        owner.VertexArray.AddSeveral(pointer);
-        owner.VertexGroups.Add(group);
-        return group;
-    }
-
     public static VertexGroup CreateRoundedRectangle(GeometryShape owner, Point2 location, Size2 size, Color color,
         float radius)
     {
         var group = new VertexGroup(owner);
-        const int quality = 32;
-
-        var startIndex = owner.VertexArray.Length;
-        Span<Vertex> pointer = stackalloc Vertex[quality + 1];
-
-        group.StartIndex = startIndex;
-        group.Length = quality + 1;
-
-        Span<Point2> centersSpan = stackalloc Point2[5];
-        centersSpan[0] = new Point2(size.Width - radius, size.Height - radius);
-        centersSpan[1] = new Point2(radius, size.Height - radius);
-        centersSpan[2] = new Point2(radius, radius);
-        centersSpan[3] = new Point2(size.Width - radius, radius);
-        centersSpan[4] = new Point2(size.Width - radius, size.Height - radius);
-
-        var arcQuality = quality / 4;
-        var da = 2.0f * MathF.PI / (quality - 4);
-
-        for (var i = 0; i <= quality; i++)
-        {
-            var cornerIdx = i / arcQuality;
-            var angle = da * (i - cornerIdx);
-
-            pointer[i].Position =
-                location + centersSpan[cornerIdx] + radius * new Point2(MathF.Cos(angle), MathF.Sin(angle));
-            pointer[i].Color = color;
-        }
-
-        owner.VertexArray.AddSeveral(pointer);
-        owner.VertexGroups.Add(group);
-        return group;
-    }
-
-    public static VertexGroup CreateRoundedRectangleAuto(GeometryShape owner, Point2 location, Size2 size, Color color,
-        float radius)
-    {
-        var group = new VertexGroup(owner);
-        var quality = (int)(radius * 2);
+        radius = float.Clamp(radius, 0, float.Min(size.Width, size.Height) / 2);
+        var quality = (int)float.Clamp(radius * 4 * 0.8f, 16, float.MaxValue);
         if (quality % 4 != 0) quality += 4 - quality % 4;
-        // Console.WriteLine("RESULT QUALITY = {0}", quality);
 
         var startIndex = owner.VertexArray.Length;
-        Span<Vertex> pointer = stackalloc Vertex[quality + 1];
-
         group.StartIndex = startIndex;
         group.Length = quality + 1;
 
@@ -99,53 +34,23 @@ public class VertexGroup
         centersSpan[3] = new Point2(size.Width - radius, radius);
         centersSpan[4] = new Point2(size.Width - radius, size.Height - radius);
 
-        var arcQuality = quality / 4;
-        var da = 2.0f * MathF.PI / (quality - 4);
-
-        for (var i = 0; i <= quality; i++)
-        {
-            var cornerIdx = i / arcQuality;
-            var angle = da * (i - cornerIdx);
-            
-            pointer[i].Position =
-                location + centersSpan[cornerIdx] + radius * new Point2(MathF.Cos(angle), MathF.Sin(angle));
-            pointer[i].Color = color;
-        }
-
-        owner.VertexArray.AddSeveral(pointer);
-        owner.VertexGroups.Add(group);
-        return group;
-    }
-    
-    public static VertexGroup CreateRoundedRectangleAuto(GeometryShape owner, Point2 location, Size2 size, Color color,
-        float radius, int quality)
-    {
-        var group = new VertexGroup(owner);
-        //Console.WriteLine("RESULT QUALITY = {0}", quality);
-
-        var startIndex = owner.VertexArray.Length;
         Span<Vertex> pointer = stackalloc Vertex[quality + 1];
 
-        group.StartIndex = startIndex;
-        group.Length = quality + 1;
-
-        Span<Point2> centersSpan = stackalloc Point2[5];
-        centersSpan[0] = new Point2(size.Width - radius, size.Height - radius);
-        centersSpan[1] = new Point2(radius, size.Height - radius);
-        centersSpan[2] = new Point2(radius, radius);
-        centersSpan[3] = new Point2(size.Width - radius, radius);
-        centersSpan[4] = new Point2(size.Width - radius, size.Height - radius);
-
         var arcQuality = quality / 4;
         var da = 2.0f * MathF.PI / (quality - 4);
 
         for (var i = 0; i <= quality; i++)
         {
             var cornerIdx = i / arcQuality;
+            var currentCenter = centersSpan[cornerIdx];
             var angle = da * (i - cornerIdx);
 
-            pointer[i].Position =
-                location + centersSpan[cornerIdx] + radius * new Point2(MathF.Cos(angle), MathF.Sin(angle));
+            var (sin, cos) = MathF.SinCos(angle);
+
+            pointer[i].Position = new Point2(
+                location.X + currentCenter.X + radius * cos,
+                location.Y + currentCenter.Y + radius * sin);
+
             pointer[i].Color = color;
         }
 
